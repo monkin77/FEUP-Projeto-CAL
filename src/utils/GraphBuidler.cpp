887509af7f5e2632c25ca543da7b3948d14f066c
up201906682 <up201906682@fe.up.pt>
2021-05-03 21:14:07 +0100

    Simplified construction

--- src/utils/GraphBuidler.cpp
+++ src/utils/GraphBuidler.cpp
@@ -6,11 +6,7 @@
 using namespace std;
 
 void defaultGraphBuilder(Graph &G){
-    Position *p1 = new Position(1, 1);
-    Position *p2 = new Position(2, 2);
-    Position *p3 = new Position(3, 3);
-
-    G.addVertex(*p1);
-    G.addVertex(*p2);
-    G.addVertex(*p3);
+    G.addVertex(Position(1, 1));
+    G.addVertex(Position(2, 2));
+    G.addVertex(Position(3, 3));
 }
