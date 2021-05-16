d6e5cca350912d6d48dcf2d57d15e3bd1bfc5e28
Bruno Rosendo <up201906334@fe.up.pt>
2021-05-16 18:50:38 +0100

    Graph Viewer integration

--- src/bakery/Interface.h
+++ src/bakery/Interface.h
@@ -2,12 +2,17 @@
 #define FEUP_PROJETO_CAL_INTERFACE_H
 
 #include "Bakery.h"
+#include "graphviewer.h"
+
+using NodeGV = GraphViewer::Node;
+using EdgeGV = GraphViewer::Edge;
 
 using namespace std;
 
 class Interface {
 private:
     Bakery* bakery;
+    GraphViewer gv;
     int selectedPhase;
 public:
     Interface();
