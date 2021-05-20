610418beb9f286657603ad761e05dbbe63873a54
Domingos Santos (up201906680) <domingosjmsantos@gmail.com>
2021-05-20 17:40:41 +0100

    Input generator

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 
 #include "bakery/Interface.h"
 #include "graphviewer.h"
+#include "bakery/InputGenerator.h"
 
 int main() {
-    Interface interface;
+    /*Interface interface;
     interface.start();
     interface.showResultGraphViewer();
+*/
+    InputGenerator gen;
+    gen.generateBakeryInput();
 
     return 0;
 }
