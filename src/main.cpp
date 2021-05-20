bb76cf38252969b1a250108263af76dad6f23405
up201906682 <up201906682@fe.up.pt>
2021-05-20 20:33:44 +0100

    Added fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 
 #include "bakery/Interface.h"
-#include "graphviewer.h"
 #include "bakery/InputGenerator.h"
 
 int main() {
 
     Interface interface;
     interface.start();
-    interface.showResultGraphViewer();
 
     /*
     InputGenerator gen;
